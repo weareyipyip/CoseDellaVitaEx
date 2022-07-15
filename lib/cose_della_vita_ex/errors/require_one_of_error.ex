a6a82043efc808662c8f6922986194e9bd23f49f
Louis Matthijssen <l.matthijssen@yipyip.nl>
2022-07-15 11:07:14 +0200

    Add custom Ecto validation to error

--- lib/cose_della_vita_ex/errors/require_one_of_error.ex
+++ lib/cose_della_vita_ex/errors/require_one_of_error.ex
@@ -7,6 +7,8 @@ defmodule CoseDellaVitaEx.Errors.RequireOneOfError do
   """
   use Absinthe.Schema.Notation
 
+  alias Ecto.Changeset
+
   defstruct [:fields, :path, message: @message, error_type: :require_one_of_error]
 
   @type t :: %__MODULE__{}
@@ -17,5 +19,18 @@ defmodule CoseDellaVitaEx.Errors.RequireOneOfError do
     field(:fields, list_of(non_null(:string)))
     interface(:error)
     import_fields(:error)
+  end
+
+  @doc """
+  Validate if the changeset has at least one of the fields set.
+  """
+  @spec validate(Changeset.t(), atom(), list(atom())) :: Changeset.t()
+  def validate(changeset, key, fields) do
+    if Enum.all?(fields, fn field -> is_nil(Changeset.get_field(changeset, field)) end) do
+      changeset
+      |> Changeset.add_error(key, @message, custom_validation: :require_one_of, fields: fields)
+    else
+      changeset
+    end
   end
 end
